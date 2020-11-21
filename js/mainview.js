323e1adce8a7f89c1d85bfe2e48363b781756576
HycFancy <yuchao.huang@vipl.ict.ac.cn>
2020-11-21 14:15:42 +0800

    TEST

--- js/mainview.js
+++ js/mainview.js
@@ -1,6 +1,7 @@
 var together = new Date(2020, 9 - 1, 07);       // start from 0
 var dateCalcH = 30;
 var dateCalcW = 800;
+var currentPage = 0;
 
 
 $(document).ready(function() {
@@ -16,17 +17,68 @@ $(document).ready(function() {
         'left': parseInt((winW - dateCalcW) / 2)
     });
     
+    // set clock
     var w = $('.clock').innerWidth();
     var h = $('.clock').innerHeight();
     $('.clock').css({
         'top' : parseInt((winH - h) / 3),
         'left': parseInt((winW - w) / 2)
     });
-    
-    timer();
 
+    // set scroll event
+    var wheelScrolling = function(event) {
+        var delta = 0;
+        if (event.wheelDelta) {
+            delta = event.wheelDelta / 120;
+        }
+        else if (event.detail) {
+            delta = -event.detail / 3;
+        }
+        if (delta) {
+            ChangePage();
+        }
+        if (event.preventDefault) {
+            event.preventDefault();
+        }
+        event.returnValue = false;
+    };
+
+    if (window.addEventListener) {
+        window.addEventListener("DOMMouseScroll", wheelScrolling, false);
+    }
+    window.onmousewheel = wheelScrolling;
+
+
+    timer();
     
 });
+
+function ChangePage() {
+    currentPage = 1 - currentPage;
+    if (currentPage == 0) {
+        SetChildrenProperty("#main-view", "display", "block");
+        SetChildrenProperty("#next-view", "display", "none");
+        $('.clock').css("display", "flex");
+        $('#main-view-flag').css({
+            "checked": true,
+            //"display": none
+        });
+        $('#next-view-flag').css("checked", false);
+    }
+    else {
+        SetChildrenProperty("#main-view", "display", "none");
+        SetChildrenProperty("#next-view", "display", "block");
+        $('#main-view-flag').css("checked", false);
+        $('#next-view-flag').css({
+            "checked": true,
+           // "display": none
+        });
+    }
+}
+
+function SetChildrenProperty(curElement, property, value) {
+    $(curElement).children().css(property, value);        
+}
 
 function timer() {
     $('#date-calc').html(GetDateDiffString());
